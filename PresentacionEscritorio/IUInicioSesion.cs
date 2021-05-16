dd7da026ab80afe05a3fc207a03feee031714c4c
Miguel Mena <menatabor09@gmail.com>
2021-05-16 18:49:26 -0500

    Primer versión del proyecto con interfaces IUInicioSesion y IURegistroUsuario

---
+++ PresentacionEscritorio/IUInicioSesion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentacionEscritorio
+{
+    public partial class IUInicioSesion : Form
+    {
+        public IUInicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCrearCuenta_Click(object sender, EventArgs e)
+        {
+            IURegistroUsuario IUCrearCuenta = new IURegistroUsuario();
+            IUCrearCuenta.Visible = true;
+            this.Visible = false;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ PresentacionEscritorio/IURegistroUsuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentacionEscritorio
+{
+    public partial class IURegistroUsuario : Form
+    {
+        public IURegistroUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void lblIniciarSesion_Click(object sender, EventArgs e)
+        {
+            IUInicioSesion IUIniciar = new IUInicioSesion();
+            IUIniciar.Visible = true;
+            this.Close();
+        }
+    }
+}
